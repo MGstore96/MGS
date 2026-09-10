8358d5f69695251ae9c10cc5704bd6520de54122
MGstore96 <akuntvcoocaa12@gmail.com>
2026-09-11 06:24:02 +0700

    update 900/lapse.js

--- 900/lapse.js
+++ 900/lapse.js
@@ -1695,7 +1695,7 @@ export async function kexploit() {
     }
     
     if (localStorage.ExploitLoaded === "yes" && sessionStorage.ExploitLoaded != "yes") {
-        msgs.innerHTML = "GoldHEN sudah Dimuat ...";
+        msgs.innerHTML ='<span style="color: #10e610;">GoldHEN Sudah Dimuat ...</span>';
         return new Promise(() => {});
     }
  
@@ -1831,7 +1831,7 @@ function runPayload(PLfile) {
 kexploit().then(() => {
 	setTimeout(() => {
 		runPayload("./goldhen_2.4b18.10.bin");
-		msgs.innerHTML = "GoldHEN v2.4b18.10 Dimuat ...";
+		msgs.innerHTML ='<span style="color: #10e610;">GoldHEN v2.4b18.10 Dimuat ...</span>';
 	},500);
 }).catch(() => {
     msgs.innerHTML = "Gagal Memuat! Mulai Ulang Konsol Anda ...";
