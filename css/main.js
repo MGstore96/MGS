5d0c17eed8f8f3f836c890cf80a9f73d851006ad
MGstore96 <akuntvcoocaa12@gmail.com>
2026-09-11 06:07:00 +0700

    update css/main.js

--- css/main.js
+++ css/main.js
@@ -53,7 +53,7 @@ async function doJb() {
     }
 
     if (fn.setuid.invoke(0) !== -1) {
-      msgs.innerHTML = "GoldHEN sudah Dimuat ...";
+      msgs.innerHTML ='<span style="color: #10e610;">GoldHEN v2.4b18.10 Dimuat ...</span>';
       return;
     }
 
