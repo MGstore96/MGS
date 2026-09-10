7744eef522574f154c44638585b137ae7ab88bff
MGstore96 <akuntvcoocaa12@gmail.com>
2026-09-11 05:55:39 +0700

    update slopkit/chain_poops.js

--- slopkit/chain_poops.js
+++ slopkit/chain_poops.js
@@ -41,7 +41,7 @@ const STOP_BEFORE_DOUBLE = params.get("stop") === "beforedouble";
 function hostOk() {
     var m = document.getElementById("msgs");
     if (m) {
-        m.innerHTML = "GoldHEN v2.4b18.10 Dimuat ...";
+        m.innerHTML ='<span style="color: #10e610;">GoldHEN v2.4b18.10 Dimuat ...</span>';
     }
 }
 
